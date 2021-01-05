8b8b65f387606dc07ec2aa06d6ab98d008e5adaf
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-05 09:13:04 +0530

    committing README.md & numpy code

---
+++ 01_Numpy_Training/01_numpy_intro.py
@@ -0,0 +1,13 @@
+"""
+Introduction to Numpy
+    Numpy is a package designed for data processing, using multi dimensional arrays.
+    It is faster than lists, because:
+        -It takes a fixed type
+        -It takes less bytes of memory.(Faster to Read).
+        -No type checking(since datatype in np array is fixed).
+        -It allocates contiguous memory (Traversing through memory is faster).
+    
+    Op Possible in Lists: insertion, deletion, appending, concatenation etc.
+    Op Possible in Numpy: lots more :P. 
+"""
+import numpy as np
