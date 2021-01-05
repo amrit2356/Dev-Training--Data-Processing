8d019cbfbb65a59d210a18f791579635560ce0b5
Amritanshu Pandey <amritanshupandey.meritocrat@gmail.com>
2021-01-05 10:56:53 +0530

    adding numpy and flake8

--- 01_Numpy_Training/01_numpy_intro.py
+++ 01_Numpy_Training/01_numpy_intro.py
@@ -16,5 +16,23 @@ a = np.array([1, 2, 3])
 print(a)
 
 # 2-D numpy Array
-b = np.array([[1, 2, 3], [4, 5, 6]])
+b = np.array([[1, 2, 3], [4, 5, 6]],dtype = 'int16')
 print(b)
+
+# Get Dimensions of Numpy Array
+print("Dimension of Numpy Array {}".format(b.ndim))
+
+# Get Shape(rows,col) of the Numpy Array
+rows, col = b.shape
+print("Rows: {} and Columns {}".format(rows, col))
+
+# Get Datatype of the Numpy Array
+data_type = b.dtype 
+print("Datatype of the given Numpy array: {}".format(data_type))
+
+# Get item size of numpy array
+print("Memory Consumed by an item in np array: {}".format(b.itemsize))
+
+# Get total size of the numpy array
+print("No of Elements: {}".format(b.size))
+print("Total Size of Numpy Array: {}".format(b.size*b.itemsize))
---
+++ 01_Numpy_Training/02_numpy_access_modify_elements.py
@@ -0,0 +1,71 @@
+"""
+Accessing/Changing specific elements, rows, columns
+"""
+
+import numpy as np
+
+# Creation of 2-D Array
+a = np.array([[1, 2, 3, 4, 5, 6, 7], [8, 9, 10, 11, 12, 13, 14]])
+
+"""
+Indexing in Numpy: [row,col]
+    row -> It points to the row index
+    col -> It points to the column index.
+
+Slicing in Numpy:
+    ':' - this operator is used to select series of elements between 2 indices.
+        - also used in conjunction to act as a stepsize.
+
+    Format:
+    [rowstart: rowend: row_stepsize, colstart: colend: col_stepsize]
+
+Traversal in Numpy:
+    +ve Index Value: 0 to n-1
+    -ve Index Value: -1 to -(n-2)
+
+Note: if you wanna traverse or print output in reverse order, use -ve Index.
+"""
+
+
+# Getting a specific element from numpy array
+# indexing starts from 0 to n-1
+print("Element in 2nd Row, 6th Column is: {}".format(a[1, 5]))
+
+# Getting a specific row:
+print("\n")
+print("Elements in 1st Row: {}".format(a[0, :]))
+print("Elements in 2nd Row: {}".format(a[1, :]))
+
+# Getting a specific column:
+print("\n")
+print("Elements in 4th Column: {}".format(a[:, 3]))
+print("Elements in 7th Column: {}".format(a[:, 6]))
+
+# Numpy Slicing
+# [start_index:end_index:stepsize]
+print("\n")
+print("Element in the first Row,slicing even numbers: {} ".format(a[0, 1:6:2]))
+
+# Slicing using -ve numbers
+print("\n")
+rev_ind = a[0, :-1]
+slice_rev = a[0, 1:-1:2]
+print("-ve index to travel np array in rev order a[0,:-1]: {}".format(rev_ind))
+print("slicing set of values(using -ve slicing): {}".format(slice_rev))
+
+
+"""
+Modification of element value in Numpy Array by:
+    - specifying the row & index value.
+    - using ':' operator, to change  different sets of elements.
+"""
+
+
+# Modifying an element in a specific function
+a[1, 5] = 20
+print("\n")
+print("Modified element in 2nd Row, 6th Column: {}".format(a))
+
+# Modifying a series of elements:
+a[:, 2] = [1, 2]
+print("Modifying Series of elements: {}".format(a))
